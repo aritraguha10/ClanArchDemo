230e474ad1fe4b5dcc80a4cb7c97b8f8e9b5bde3
Aritra Guha <Aritra.Guha@hyland.com>
2021-06-14 20:05:53 +0530

    Initial Commit for Data Project, Update MVC iwth new UniversityDBContext

--- CleanArch/CleanArch.MVC/Startup.cs
+++ CleanArch/CleanArch.MVC/Startup.cs
@@ -1,3 +1,4 @@
+using CleanArch.Data;
 using CleanArch.MVC.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -31,6 +32,11 @@ namespace CleanArch.MVC
                 options.UseSqlServer(
                     Configuration.GetConnectionString("UniversityIdentityDBConnection")));
             services.AddDatabaseDeveloperPageExceptionFilter();
+
+            services.AddDbContext<UniversityDBContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("UniversityDBConnection"));
+            });
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
